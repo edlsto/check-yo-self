f3815bd6339fd840e3003c11507274731fc31553
Edward Stoner <edwardlstoner@gmail.com>
2020-01-12 14:14:32 -0700

    Edit methods for updating todos

--- todo-list.js
+++ todo-list.js
@@ -1,8 +1,8 @@
 class ToDoList {
-  constructor(title, tasks) {
-    this.id = new Date().valueOf();
+  constructor(title, tasks, id, urgent) {
+    this.id = id || new Date().valueOf();
     this.title = title;
-    this.urgent = false;
+    this.urgent = urgent || false;
     this.tasks = tasks;
   }
 
@@ -14,16 +14,34 @@ class ToDoList {
 
   deleteFromStorage() {
     var allTaskLists = JSON.parse(localStorage.getItem('allTaskLists')) || [];
-    allTaskLists = reinstantiateAllTasksList(allTaskLists);
-    allTaskLists.splice(allTaskLists.indexOf(this), 1);
+    for (var i = 0; i < allTaskLists.length; i++) {
+      if (this.id === allTaskLists[i].id){
+        allTaskLists.splice(i, 1)
+      }
+    }
     localStorage.setItem('allTaskLists', JSON.stringify(allTaskLists));
   }
 
+  updateTask(taskId) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].id === taskId) {
+        console.log(this.tasks[i].text)
+        this.tasks[i].text 
+      }
+    }
+  }
+
   updateToDo() {
+    var allTaskLists = JSON.parse(localStorage.getItem('allTaskLists')) || [];
+    for (var i = 0; i < allTaskLists.length; i++) {
+      if (this.id === allTaskLists[i].id){
+        allTaskLists[i].title = this.title;
+        allTaskLists[i].urgent = this.urgent;
+        allTaskLists[i].tasks = this.tasks;
+      }
+    }
+    localStorage.setItem('allTaskLists', JSON.stringify(allTaskLists));
+      }
 
-  }
 
-  updateTask() {
-
-  }
 }
