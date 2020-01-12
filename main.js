5473467da32450d94b82e12e6004854a697ae089
Edward Stoner <edwardlstoner@gmail.com>
2020-01-11 22:11:15 -0700

    Add checkmark functionality

--- main.js
+++ main.js
@@ -5,31 +5,131 @@ var taskListContainer = document.querySelector('.task-list-inner');
 var makeTaskList = document.querySelector('.make-task-list');
 var taskTitleInput = document.querySelector('.dashboard-input');
 var cardsSection = document.querySelector('.cards')
+var grid = document.querySelector(".cards");
 
+taskTitleInput.addEventListener('keyup', validateMakeTaskList)
+makeTaskList.addEventListener('click', createTaskList)
+addTaskInput.addEventListener('keyup', validateTaskInput)
 addTaskButton.addEventListener('click', addTask);
+
 taskListContainer.addEventListener('click', function(){
   removeTask(event);
 });
 
 cardsSection.addEventListener('click', function(){
   deleteCard(event);
+  checkOffTask(event);
 })
+
+loadCards();
+
+function addTask() {
+  var task = new Task(addTaskInput.value)
+  tasks.push(task);
+  taskListContainer.innerHTML +=
+  `<li><img src="./assets/delete.svg" class="delete-task">${task.text}</li>`
+  addTaskInput.value = '';
+  addTaskButton.setAttribute('disabled', 'disabled');
+  validateMakeTaskList()
+}
+
+function checkForEmpty(list) {
+  if (list.length === 0){
+    cardsSection.classList.add('empty');
+    cardsSection.innerHTML = '<h3>Create a to-do!</h3>';
+  }
+}
+
+function checkOffTask(event) {
+  if (event.target.classList.contains('checkbox')) {
+    toggleCheck();
+    var taskId = parseInt(event.target.id);
+    var cardId = parseInt(event.target.parentElement.parentElement.parentElement.parentElement.id);
+    var allTaskLists = getAllSavedTasks();
+    var matchedTaskList = allTaskLists.filter(taskList => taskList.id === cardId)[0];
+    matchedTaskList.updateTask(taskId);
+    matchedTaskList.updateToDo();
+  }
+}
+
+function createTaskList() {
+  var taskList = new ToDoList(taskTitleInput.value, tasks);
+  taskList.saveToStorage();
+  taskTitleInput.value = '';
+  tasks = [];
+  taskListContainer.innerHTML = '';
+  cardsSection.classList.remove('empty');
+  loadCards();
+  resizeAllGridItems();
+  validateMakeTaskList();
+}
+
+function deleteCard(e) {
+  if (e.target.parentElement.classList.contains('card-delete-icon')) {
+    var card = e.target.parentElement.parentElement.parentElement;
+    card.remove()
+    var allTaskLists = getAllSavedTasks();
+    allTaskLists.forEach(function(taskList, i){
+      if (taskList.id === parseInt(card.id)) {
+        taskList.deleteFromStorage();
+      }
+    })
+    allTaskLists = getAllSavedTasks();
+    checkForEmpty(allTaskLists);
+  }
+}
+
+function getAllSavedTasks() {
+  var allTaskLists = JSON.parse(localStorage.getItem('allTaskLists')) || [];
+  allTaskLists = reinstantiateAllTasksList(allTaskLists);
+  return allTaskLists;
+}
+
+function loadCards() {
+  var allTaskLists = getAllSavedTasks();
+  cardsSection.innerHTML = renderCardsHTML(allTaskLists);
+  checkForEmpty(allTaskLists);
+}
 
 function makeTaskListHTML(taskList) {
   var taskListHTML = '';
   for (var i = 0; i < taskList.tasks.length; i++) {
     var task = taskList.tasks[i].text;
-    var html = `<li><img src="./assets/checkbox.svg">${task}</li>`
+    var html = `<li><img src="./assets/${taskList.tasks[i].done === false ? 'checkbox' : 'checkbox-active'}.svg" class="checkbox${taskList.tasks[i].done === false ? '' : ' checked'}" id="${taskList.tasks[i].id}">${task}</li>`
     taskListHTML += html;
   }
   return taskListHTML;
+}
+
+function reinstantiateAllTasksList(allTaskLists) {
+  var allTaskListsWithMethods = [];
+  allTaskLists.forEach(function(taskList){
+    allTaskListsWithMethods.push(new ToDoList(taskList.title, taskList.tasks, taskList.id));
+  })
+  return allTaskListsWithMethods;
+}
+
+function removeTask(e) {
+  if (e.target.classList.contains('delete-task')) {
+    removeTaskFromTasks(e);
+    e.target.parentNode.remove();
+  };
+  validateMakeTaskList()
+}
+
+function removeTaskFromTasks(e) {
+  tasks.forEach(function(task, i){
+    if (e.target.parentElement.innerText === task.text) {
+      tasks.splice(i, 1)
+    };
+  });
 }
 
 function renderCardsHTML(allTaskLists) {
   var cardsHTML = ''
   for (var i = allTaskLists.length - 1; i >= 0; i--){
     cardsHTML +=
-    `<div class="card">
+    `<div class="card" id="${allTaskLists[i].id}">
         <h2>${allTaskLists[i].title}</h2>
         <div class="content">
           <ul>
@@ -51,54 +151,12 @@ function renderCardsHTML(allTaskLists) {
   return cardsHTML;
 }
 
-taskTitleInput.addEventListener('keyup', validateMakeTaskList)
-
-makeTaskList.addEventListener('click', createTaskList)
-
-addTaskInput.addEventListener('keyup', validateTaskInput)
-
-function loadCards() {
-  cardsSection.classList.remove('empty');
-  var allTaskLists = JSON.parse(localStorage.getItem('allTaskLists')) || [];
-  allTaskLists = reinstantiateAllTasksList(allTaskLists);
-  cardsSection.innerHTML = renderCardsHTML(allTaskLists);
-}
-
-loadCards();
-
-function reinstantiateAllTasksList(allTaskLists) {
-  var allTaskListsWithMethods = [];
-  allTaskLists.forEach(function(taskList){
-    allTaskListsWithMethods.push(new ToDoList(taskList.title, taskList.tasks));
-  })
-  return allTaskListsWithMethods;
-}
-
-function deleteCard(e) {
-  if (e.target.parentElement.classList.contains('card-delete-icon')) {
-    var title = e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.innerText;
-    var card = e.target.parentElement.parentElement.parentElement;
-    var allTaskLists = JSON.parse(localStorage.getItem('allTaskLists')) || [];
-    allTaskLists = reinstantiateAllTasksList(allTaskLists);
-    allTaskLists.forEach(function(taskList){
-      if (taskList.title === title) {
-        card.remove();
-        taskList.deleteFromStorage();
-      }
-    })
+function validateMakeTaskList() {
+  if (tasks.length > 0 && taskTitleInput.value !== '') {
+    makeTaskList.removeAttribute('disabled')
+  } else {
+    makeTaskList.setAttribute('disabled', 'disabled');
   }
-}
-
-function createTaskList() {
-  var taskList = new ToDoList(taskTitleInput.value, tasks);
-  taskList.saveToStorage();
-  taskTitleInput.value = '';
-  tasks = [];
-  taskListContainer.innerHTML = '';
-  cardsSection.classList.remove('empty');
-  loadCards();
-  resizeAllGridItems();
-  validateMakeTaskList();
 }
 
 function validateTaskInput() {
@@ -109,43 +167,28 @@ function validateTaskInput() {
   }
 }
 
-function validateMakeTaskList() {
-  if (tasks.length > 0 && taskTitleInput.value !== '') {
-    makeTaskList.removeAttribute('disabled')
+
+
+
+
+
+
+
+
+function toggleCheck() {
+  if (event.target.classList.contains('checked')) {
+    event.target.classList.remove('checked');
+    event.target.src = './assets/checkbox.svg';
   } else {
-    makeTaskList.setAttribute('disabled', 'disabled');
+    event.target.classList.add('checked')
+    event.target.src = './assets/checkbox-active.svg';
   }
 }
 
-function addTask() {
-  var task = new Task(addTaskInput.value)
-  tasks.push(task);
-  taskListContainer.innerHTML +=
-  `<li><img src="./assets/delete.svg" class="delete-task">${task.text}</li>`
-  addTaskInput.value = '';
-  addTaskButton.setAttribute('disabled', 'disabled');
-  validateMakeTaskList()
-}
-
-function removeTaskFromTasks(e) {
-  tasks.forEach(function(task, i){
-    if (e.target.parentElement.innerText === task.text) {
-      tasks.splice(i, 1)
-    };
-  });
-}
-
-function removeTask(e) {
-  if (e.target.classList.contains('delete-task')) {
-    removeTaskFromTasks(e);
-    e.target.parentNode.remove();
-  };
-  validateMakeTaskList()
-
-}
 
 
-var grid = document.querySelector(".cards");
+
+
 
 grid.addEventListener("resize", resizeAllGridItems);
 
